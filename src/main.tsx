f429c29e94905a245f9ea7c53e75ab5bab3f881e
lipcyy <ripzzbg@gmail.com>
2026-09-08 05:17:11 +0700

    feat: route landing and sandbox separately

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App'
+import Sandbox from './Sandbox'
+
+const isSandbox = window.location.pathname.replace(/\/$/, '') === '/sandbox'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isSandbox ? <Sandbox /> : <App />}
   </StrictMode>,
 )
